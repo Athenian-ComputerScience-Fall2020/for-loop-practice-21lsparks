b5ac83c050905490a4f991c0f66d0b1027c0777e
21lsparks <21lsparks@athenian.org>
2020-09-13 20:41:03 +0000

    added link

--- divisibility_check.py
+++ divisibility_check.py
@@ -1,5 +1,5 @@
 # Collaborators: 
-#
+# https://www.tutorialspoint.com/python-program-to-print-all-the-numbers-divisible-by-3-and-5-for-a-given-number
 
 for x in range(2, 50)
     if number = (x%3=0)
