7008cdc59f509a6aa63260793360f4443e4146d0
21lsparks <21lsparks@athenian.org>
2020-09-24 21:16:06 +0000

    IT WORKS

--- divisibility_check.py
+++ divisibility_check.py
@@ -1,8 +1,8 @@
 # Collaborators: 
 # https://www.tutorialspoint.com/python-program-to-print-all-the-numbers-divisible-by-3-and-5-for-a-given-number
 
-for x in range(2, 50)
-    if number = (x%3=0)
-    print("'number' is divisible by 3")
+for x in range(2, 50):
+    if x%3==0:
+        print(f"{x} is divisible by 3")
     else:
-    print "x is not divisible by 3"
+        print (f"{x} is not divisible by 3")
